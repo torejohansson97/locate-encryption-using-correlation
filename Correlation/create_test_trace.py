eb307b5c8182f09c13aa7c852d144c6c062a147e
Simon Weideskog <simonweideskog@live.se>
2021-02-22 16:28:41 +0100

    New test trace

--- Correlation/create_test_trace.py
+++ Correlation/create_test_trace.py
@@ -2,10 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import read
 
+def normMaxMin(inputArray):
+	max = np.max(inputArray)
+	min = np.min(inputArray)
+	return np.array([(x - min) / (max - min) for x in inputArray])
+
 def soundNoise(filename):
 	sound = read(filename)
 	array = np.array(sound[1],dtype=float)
-	return array
+	return normMaxMin(array)
 
 def whiteNoise(length, mean = 0, std = 1):
 	return np.random.normal(mean, std, size=length)
@@ -13,6 +18,11 @@ def whiteNoise(length, mean = 0, std = 1):
 def insertTrace(trace, testTrace, offset):
 	for i in range(len(trace)):
 		testTrace[i+offset] = trace[i]+testTrace[i+offset]
+	return testTrace
+
+def insertTraceAverage(trace, testTrace, offset):
+	for i in range(len(trace)):
+		testTrace[i+offset] = (trace[i]+testTrace[i+offset])/2
 	return testTrace
 
 def addSamples(trace1, trace2):
@@ -24,16 +34,14 @@ def main():
 	sNoise = soundNoise('../data/baloobas.wav')
 	wNoise = whiteNoise(len(sNoise))
 	testTrace = addSamples(wNoise, sNoise)
+	testTrace_scaled = normMaxMin(testTrace)
+
 	trace = np.load('../data/ff-em-sca-data/for_testing/3m/10k_d6/1avg/nor_traces_maxmin.npy')[0]
-	max = np.max(testTrace)
-	min = np.min(testTrace)
-	testTrace_scaled = np.array([(x - min) / (max - min) for x in testTrace])
-	final = insertTrace(trace, testTrace_scaled, 100000)
-	max = np.max(final)
-	min = np.min(final)
-	finalmaxmin = np.array([(x - min) / (max - min) for x in final])
+
+	final = insertTrace(trace, testTrace, 100000)
+	finalmaxmin = normMaxMin(final)
 	np.save('test_trace.npy', finalmaxmin)
-	plt.plot(finalmaxmin[100000:100500])
+	plt.plot(finalmaxmin[99000:101000])
 	plt.show()
 
 
