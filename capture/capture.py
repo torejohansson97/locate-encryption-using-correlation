e1650c65c26aa0e0ba9a5f9fd295ec0c617c098b
torejohansson97 <>
2021-03-18 09:59:49 +0100

    Made it work

--- capture/capture.py
+++ capture/capture.py
@@ -53,7 +53,7 @@ class capture(gr.top_block):
         self.uhd_usrp_source_0.set_samp_rate(samp_rate)
         # No synchronization enforced.
         self.dc_blocker_xx_0 = filter.dc_blocker_ff(32, True)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, '../data/' + outfile, False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, outfile, False)
         self.blocks_file_sink_0.set_unbuffered(False)
         self.analog_am_demod_cf_0 = analog.am_demod_cf(
         	channel_rate=samp_rate,
--- capture/main.py
+++ capture/main.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import sys, getopt
+import os
 import serial
 import time
+import numpy as np
 
 from capture import *
 
 def main():
-    outputfile='out'
+    outputfile = '../data/our-data/raw'
     targetDevice = '/dev/ttyACM0'
     repetiotion = 100
-    key = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,23,0]
-    text = [0,0,0,0,0,0,0,0,0,25,0,0,0,0,0,0]
-
+    key = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    plaintext = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    count=0
     recorder = capture(outfile=outputfile)
     
     dev = serial.Serial(targetDevice, baudrate=115200, timeout=20)
@@ -24,20 +26,54 @@ def main():
 
     enterTinyAES(dev)
     setRepetition(dev, repetiotion)
-    setKey(dev, key)
-    setPlainText(dev, text)
     
-    recorder.start()
-
-    runEncryption(dev)
+    for i in range(1,4):
+        #dir = '../data/our-data/100k_d10_k' + str(i) + '_100avg'
+        dir = '../data/our-data/test/k' + str(i)
+        key = np.load(dir + '/key.npy')
+        plaintext = np.load(dir + '/pt.npy')
+        setKey(dev, key)
+        for text in plaintext:
+            print(str(count))
+            setPlainText(dev, text)
+            recorder.start()
+            runEncryption(dev)
+            recorder.stop()
+            recorder.wait()
+            appendRawtoArray(outputfile, dir + '/traces.npy')
+            count+=1
+            recorder.blocks_file_sink_0.close()
+            os.remove(outputfile)
+            recorder.blocks_file_sink_0.open(outputfile)
     
     exitTinyAES(dev)    
     stopCarrier(dev)
 
     # Clean up and exit
-    recorder.stop()
-    recorder.wait()
     dev.close()
+
+def appendRawtoArray(rawPath, arrayPath):
+    raw = np.fromfile(rawPath, dtype='float32')
+    print('Raw length: ' + str(len(raw)))
+    raw = raw.reshape(1, len(raw))
+    try:
+        array = np.load(arrayPath)
+        length = array.shape[1]
+        diff = length - raw.shape[1]
+    
+        if diff > 0:
+            temp = np.zeros((1, array.shape[1]))
+            temp[0, :raw.shape[1]] = raw[0]
+            raw = temp
+        elif diff < 0:
+            raw = raw[:1, 0:length] # Trim the capured to equal length
+        
+        array = np.append(array, raw, axis=0)
+
+    except FileNotFoundError:
+        array = raw
+    
+    np.save(arrayPath, array)
 
 def enterTinyAES(device):
     device.write(b'n') # Enter tinyAES
@@ -48,12 +84,14 @@ def exitTinyAES(device):
     print(device.readline())
 
 def setChannel(device, channel='0'):
+    #print("Set channel")
     device.write(b'a')
     print(device.readline())
     device.write(b'0\r\n')
     print(device.readline())
 
 def setPower(device, power='0'):
+    #print("Set pwr")
     device.write(b'p0')
     print(device.readline())
     print(device.readline())
@@ -65,17 +103,20 @@ def stopCarrier(device):
     device.write(b'e')
 
 def setRepetition(device, repetiotion=2000):
-    device.write(b'n2000\r\n')
+    #print("Set rep")
+    device.write(b'n' + str(repetiotion).encode() + b'\r\n')
     print(device.readline())    
     
 def setKey(device, key):
     # Assumes i tinyAES mode
+    #print("Set key")
     command_line = '%s%s\r\n' % ('k', " ".join(str(char) for char in key))
     device.write(command_line.encode())
     print(device.readline())
 
 def setPlainText(device, text):
     # Assumes i tinyAES mode
+    #print("Set PT")
     command_line = '%s%s\r\n' % ('p', " ".join(str(char) for char in text))
     device.write(command_line.encode())
     print(device.readline())
@@ -83,7 +124,7 @@ def setPlainText(device, text):
 def runEncryption(device):
     print("Encrypting...")
     device.write(b'r')
-    print(device.readline())
+    print(device.read_until(b'Done\r\n'))
 
 if __name__ == "__main__":
    main()
